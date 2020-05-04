3db37d9daecdf3a1d9ccb2bce74eb04c257f327e
Tanvir <tanvirk7011@Gmail.com>
2020-05-04 02:59:36 +0100

    Main Menu system, Audio Manager, Player Movement have been implemented
    I should have pushed separately. Ohh and working on  saving and loading

---
+++ Assets/Scripts/scriptAudioManager.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class scriptAudioManager : MonoBehaviour
+{
+    // TODO: Make this in to a singleton class or something similar so that it could be used through all scenes
+
+    // Hold reference to this object
+    public scriptAudioManager audioManager;
+
+    // Array that stores all sounds
+    public scriptSound[] sounds;
+
+    private void Awake()
+    {
+        if (audioManager == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            audioManager = this;
+        }
+        else if (audioManager != null)
+            Destroy(audioManager);
+        
+        foreach (scriptSound sound in sounds)
+        {
+            sound.source = gameObject.AddComponent<AudioSource>();
+            sound.source.clip = sound.clip;
+            sound.source.name = sound.name;
+            sound.source.volume = sound.volume;
+            sound.source.mute = sound.mute;
+        }
+    }
+
+    public scriptSound Find(string name)
+    {
+        scriptSound soundToFind = Array.Find(sounds, clip => clip.name == name);
+        return soundToFind;
+    }
+
+    public void Play(string name)
+    {
+        Find(name).source.Play();
+    }
+
+    public bool IsPlaying(string name)
+    {
+        if (Find(name).source.isPlaying)
+            return true;
+        else
+            return false;
+    }
+
+    public void ToggleLoop(string name)
+    {
+        if (Find(name).source != null)
+        {
+            Find(name).source.loop = !Find(name).source.loop;
+        }
+    }
+
+    public void Stop(string name)
+    {
+        if (Find(name).source != null)
+        { 
+            if (Find(name).source.isPlaying)
+                Find(name).source.Stop();
+        }
+    }
+}
---
+++ Assets/Scripts/scriptGameData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.U2D.Path.GUIFramework;
+using UnityEngine;
+
+public class scriptGameData : MonoBehaviour
+{
+    // Saving and Loading Data
+    // Singleton Class
+
+    // Hold reference to this object
+    public static scriptGameData gameData;
+
+    // Variables to save
+    public float name;
+    public float health;
+    public float experience;
+    public float[] position = new float[3];
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (gameData == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            gameData = this;
+        }
+        else if (gameData != this)
+            Destroy(gameData);
+    }
+}
