39292eea799e9c811c87052b3afcc415e9ae2d95
James McGowan <43676496+m012343i@users.noreply.github.com>
2020-05-06 16:21:01 +0100

    Item Creation
    Allow me to create item with a name and an icon.

---
+++ Assets/McGowan/Scripts/ItemPickUp.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ItemPickUp : MonoBehaviour
+{
+    public Item item;
+    void OnTriggerEnter()
+    {
+        Debug.Log("Picked up" + transform.name);
+        PickUp();
+    }
+
+    void PickUp()
+    {
+        Debug.Log("Picking Up " + item.name);
+        // add item to inventory
+        Destroy(gameObject);
+    }
+}
