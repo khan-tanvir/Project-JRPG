2d949fc74997d96145d1fa602ded7d74bc65410a
James McGowan <43676496+m012343i@users.noreply.github.com>
2020-05-06 15:03:02 +0100

    Inventory System Start
    Started creating the inventory system for the RPG game. So far hae created a UI panel with 2 buttons.

---
+++ Assets/McGowan/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Target;
+
+    public float SmoothSpeed = 0.125f;
+    public Vector3 Offset;
+
+     void LateUpdate()
+    {
+        transform.position = Target.position + Offset;
+    }
+}
