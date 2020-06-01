499ec6c5a12b90bd9332ded66094f762dce7a919
csbcoates <csbcoates@users.noreply.github.com>
2020-06-02 00:02:53 +0100

    Scene Transistions

---
+++ Assets/Scripts/SceneTransitions/SceneTransistion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransistion : MonoBehaviour
+{
+    public string sceneLoad;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player") && !collision.isTrigger)
+        {
+            SceneManager.LoadScene(sceneLoad);
+        }
+    }
+
+}
