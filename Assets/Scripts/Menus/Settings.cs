5970269435de2c5947daa72d7ec78c7f56b56fd4
James McGowan <43676496+m012343i@users.noreply.github.com>
2020-06-04 12:32:40 +0100

    Redo the resolution
    Trying to do the resolution so it dosen't repeat the same values

---
+++ Assets/Scripts/Menus/Settings.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    void Awake()
+    {
+        Resolution[] resolutionsArray = Screen.resolutions;
+
+        // Print the resolutions
+        foreach (var res in resolutionsArray)
+        {
+            Debug.Log(res.width + "x" + res.height + " : " + res.refreshRate);
+        }
+    }
+}
